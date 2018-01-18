4c51daeef4f3f893fdb463ee18c91ed940e5aba1
Ambroise <ambfrom91@gmail.com>
2018-01-18 10:50:20 +0100

    Update fire.js

--- src/fire.js
+++ src/fire.js
@@ -3,12 +3,7 @@ import * as firebase from 'firebase';
 
 require("firebase/firestore");
 const config = {
-  apiKey: "AIzaSyB9oFuT0RMGqk_-Dvfc1dpwVz74tSMI_ww",
-  authDomain: "reactivation-7bfc5.firebaseapp.com",
-  databaseURL: "https://reactivation-7bfc5.firebaseio.com",
-  projectId: "reactivation-7bfc5",
-  storageBucket: "reactivation-7bfc5.appspot.com",
-  messagingSenderId: "280521103101"
+  // HÉHÉÉÉÉÉÉÉÉÉÉÉ
 };
 const fire = firebase.initializeApp(config);
 const firestore = fire.firestore();
